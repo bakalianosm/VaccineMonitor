6552d3881f474cf142b4e7f02ac03ef4c0b9d842
bakalianosm <michalianos99@gmail.com>
2021-03-08 19:10:03 +0200

    added destroy functions

--- include/generalFunctions.h
+++ include/generalFunctions.h
@@ -1,2 +1,21 @@
-#pragma once
 
+
+
+
+/* -------------------------------------------------------
+ * --------------- GENERAL FUNCTIONS ---------------------
+ * ------------------ HEADER FILE ------------------------
+ * -------------------------------------------------------
+ */
+
+#pragma once
+#include "common.h"
+
+/* A function that is used to compare two records */
+int compare_records(Pointer a, Pointer b);
+
+/* A function that is used to destroy a key of Map */
+void record_destroy_key(Pointer rec);
+
+/* A function that is used to destroy a value of Map */
+void record_destroy_value(Pointer rec);
--- modules/generalFunctions.c
+++ modules/generalFunctions.c
@@ -1,6 +1,38 @@
 
-#include <stdio.h>
-#include <string.h>
+
+
+/* --------------------------------------------------------
+ * ---------------- GENERAL FUNCTIONS ---------------------
+ * ------------------ IMPLEMENTATION ----------------------
+ * --------------------------------------------------------
+ */
+
+/* ----------- System header files  --------- */
 #include <stdlib.h>
 
+/* -------- User-defined header files ------- */
 #include "generalFunctions.h"
+#include "common.h"
+
+int compare_records(Pointer a, Pointer b){
+    return 0;
+}
+
+void record_destroy_key(Pointer rec){
+    free(((Record)rec)->virusName);
+}
+
+void record_destroy_value(Pointer rec){
+    free(((Record)rec)->firstName);
+    free(((Record)rec)->lastName);
+    free(((Record)rec)->country);
+    
+    if( ((Record)rec)->isVaccinated == true)
+        free(((Record)rec)->dateVaccinated);
+
+    free(((Record)rec));
+}
+
+
+
+
