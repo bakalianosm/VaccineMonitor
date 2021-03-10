0e75a5217349b71cf586f66b41d6573bf8ceffc0
bakalianosm <michalianos99@gmail.com>
2021-03-10 19:54:17 +0200

    added parse/assing/validate functions

--- include/generalFunctions.h
+++ include/generalFunctions.h
@@ -19,3 +19,10 @@ void record_destroy_key(Pointer rec);
 
 /* A function that is used to destroy a value of Map */
 void record_destroy_value(Pointer rec);
+
+/* A function that parses the values from each sentence from the 
+   input file to an array of strings */
+void parseValues(char buffer[], char* array[]);
+
+/* A function that set the values of records' data array to values */
+void assignValues(char* valuesArray[],int *ID,   char **firstName, char **lastName, char **country, int *age, char **virusName, char **isVaccinated, date* dateVaccinated);
--- modules/generalFunctions.c
+++ modules/generalFunctions.c
@@ -8,7 +8,9 @@
  */
 
 /* ----------- System header files  --------- */
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* -------- User-defined header files ------- */
 #include "generalFunctions.h"
@@ -33,6 +35,47 @@ void record_destroy_value(Pointer rec){
     free(((Record)rec));
 }
 
+void parseValues(char buffer[], char* array[]){
 
+    int i = 0;
+    array[i] = strtok(buffer, " ");
+    while(array[i] !=NULL ){
+        array[++i] = strtok(NULL, " ");
+    }
 
+}
 
+void assignValues(char* valuesArray[],int *ID,   char **firstName, char **lastName, char **country, int *age, char **virusName, char **isVaccinated, date* dateVaccinated){
+    *ID = atoi(valuesArray[0]);
+    
+    *firstName = strdup(valuesArray[1]);
+    *lastName = strdup(valuesArray[2]);
+    *country = strdup(valuesArray[3]);
+    *age = atoi(valuesArray[4]);
+    *virusName = strdup(valuesArray[5]);
+    *isVaccinated = strdup(valuesArray[6]);
+   
+    if (valuesArray[7] != NULL){
+        *dateVaccinated = strdup(valuesArray[7]);
+    }
+    else {
+        dateVaccinated = NULL;
+    }
+
+    /* -------------------------------- VALIDATION CHECK --------------------------------------------*/
+    
+    /* age check */
+    if(! (*age > 0 && *age <= 120)) {
+        printf("ERROR IN RECORD %d %s %s %s %d %s %s \n", *ID, *firstName, *lastName, *country, *age, *virusName, *isVaccinated);
+        if( dateVaccinated != NULL) printf("%s\n", *dateVaccinated);
+    }
+
+    /* if vaccinated is no and provided a date */
+    if( (strcmp(valuesArray[6],"NO") == 0) && valuesArray[7] != NULL)  
+        printf("ERROR IN RECORD %d %s %s %s %d %s %s %s \n", *ID, *firstName, *lastName, *country, *age, *virusName,*isVaccinated, *dateVaccinated);
+
+    /* if is vaccinated and not given a date  */
+    if( (strcmp(valuesArray[6],"YES") == 0) && ( valuesArray[7] == NULL ))
+        printf("ERROR IN RECORD %d %s %s %s %d %s %s \n", *ID, *firstName, *lastName, *country, *age, *virusName, *isVaccinated);
+
+}
