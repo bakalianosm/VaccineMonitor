73539dc4624cc44b60ae40f1c647fe9ca4592b2c
bakalianosm <michalianos99@gmail.com>
2021-03-17 15:52:54 +0200

    Skiplist implementation updated

--- include/skipList.h
+++ include/skipList.h
@@ -12,10 +12,10 @@
 #include "common.h"
 
 typedef struct skip_list* SkipList;
-typedef struct upper_node* UpperNode;
+typedef struct skip_list_node* skipListNode;
 
 /* This function creates and empty SkipList */
-SkipList SL_create(int estimatedSize, DestroyFunc destroy);
+SkipList SL_create(int maxLevel, DestroyFunc destroy_key, DestroyFunc destroy_value);
 
 /* This function is used to return the number of layers */
 int SL_layers(SkipList skiplist);
@@ -24,10 +24,25 @@ int SL_layers(SkipList skiplist);
 int SL_size(SkipList skiplist);
 
 /* This function is used to insert an item in skip list */
-void SL_insert(SkipList skiplist, Pointer value, CompareFunc compare);
+void SL_insert(SkipList skiplist, Pointer key, Pointer value, CompareFunc compare);
 
 /* This function is used to print each level of the skip list */
-void SL_print(SkipList skiplist);
+void SL_print(SkipList skiplist, PrintFunc print);
+
+/* IF node exist returns value of the first node that has "key" key based on  the compare function */
+Pointer SL_find(SkipList skiplist, Pointer key, CompareFunc compare);
+
+/* Removes node with "key" key if exists */
+void SL_remove(SkipList skiplist, Pointer key);
 
 /* This function is used to free all the memory that the list has allocated */
 void SL_destroy(SkipList skiplist);
+
+/* Returns the first node with the given "key" base on the compare function */
+skipListNode SL_find_node(SkipList skiplist, Pointer key, CompareFunc compare);
+
+/* Returns the key of the node */
+Pointer SL_node_key(skipListNode node);
+
+/* Returns the value of the node */
+Pointer SL_node_val(skipListNode node);
--- modules/skipList.c
+++ modules/skipList.c
@@ -14,44 +14,70 @@
 #include <math.h>
 #include <string.h>
 #include <time.h>
+
 /* -------- User-defined header files ------- */
 #include "common.h"
 #include "skipList.h"
 #include "linkedList.h"
 
-
+#define MAX_LVL 6
 #define INSERT 1
 #define NOT_INSERT 0
 struct skip_list {
-  
     int size;
-    int layers;
-    LinkedList bottomList;
-    LinkedList* upperLists;
-    DestroyFunc destroyItem;
-    
+    int curLevel;
+    int maxLevel;
+    skipListNode header;
+    DestroyFunc destroy_key;
+    DestroyFunc destroy_value;
+
 };
 
-struct upper_node {
-    UpperNode up;
-    UpperNode next;
-    ListNode down;
+struct skip_list_node {
+    int height;
+    Pointer key;
+    Pointer value;
+    skipListNode *next;
 };
 
-SkipList SL_create(int estimatedSize, DestroyFunc destroy){
+SkipList SL_create(int maxLevel, DestroyFunc destroy_key, DestroyFunc destroy_value){
     
-    /* allocate memory to store the skip list */
     SkipList skiplist = malloc(sizeof(*skiplist));
-
-    /* initialize values */
-    skiplist->size = 0;
-    skiplist->layers = log2((double)estimatedSize);
+    if (skiplist == NULL){
+        printf("Error with skip list creation\n");
+    }
+    skiplist->curLevel = 1;
+    skiplist->maxLevel = maxLevel;
     
-    /* after we computed the number of layers we can initialize lists*/
-    
-    skiplist->destroyItem = destroy;
+    skipListNode header = malloc(sizeof(*header));
+    header->next = malloc( (maxLevel+1) * sizeof(*(header->next)));
 
+
+    for(int i = 0 ; i<= skiplist->maxLevel ; i++){
+        header->next[i] = NULL;
+    }
+
+    skiplist->header = header;
     return skiplist;
+}
+
+int randomLevel(SkipList skiplist){
+
+    /* This is an internal function that will be used from the 
+       insert functio */
+
+    /* Its like, how many times we flip a coin and we got tails */
+  
+
+    /* At least we have one level */
+    float r = (float)rand()/RAND_MAX;
+    int level = 1;
+    while( r < 0.5 && level < skiplist->maxLevel){
+        level++;
+        r = (float)rand()/RAND_MAX;
+    }
+
+    return level;
 }
 
 int SL_size(SkipList skiplist){
@@ -59,15 +85,137 @@ int SL_size(SkipList skiplist){
 }
 
 int SL_layers(SkipList skiplist){
-    return skiplist->layers;
+    return skiplist->curLevel;
 }
 
-void SL_insert(SkipList skiplist, Pointer value, CompareFunc compare){
+void SL_insert(SkipList skiplist, Pointer key, Pointer value,CompareFunc compare){
+
+    /* array of pointer to elements that are predecessors of the new element */
+    int height = skiplist->maxLevel +1;
+
+    skipListNode update[height];
+
+    for(int i = 0 ; i < height ; i++){
+        update[i] = NULL;
+    }
+
+
+    skipListNode x = skiplist->header;
+    int i, level;
+    
+    for (i = skiplist->curLevel; i>=1 ; --i){
+        while( x->next[i]!=NULL){
+            if( compare(x->next[i]->key , key) >=0 ){
+                break;
+            }
+            x = x->next[i];
+        }
+        update[i] = x;
+    }
+    
+
+    if(x->next[1]!=NULL){
+        if (x->next[1]->key!=NULL){
+            if (compare(x->next[1]->key,key)== 0 ){
+                x->next[1]->value = value;
+                return ;
+            }
+        }
+    }
+    else {
+        int level = randomLevel(skiplist);
+
+        if (level > skiplist->curLevel){
+            for(int i= skiplist->curLevel +1 ; i<= level ; i++){
+                update[i] = skiplist->header;
+            }
+            skiplist->curLevel = level;
+        }
+
+        x = malloc(sizeof(*x));
+        x->key = key;
+        x->value = value;
+
+        x->next = malloc(sizeof(*(x->next))* (level +1));
+
+        for (int i = 1 ; i <= level ; i++){
+            x->next[i] = update[i]->next[i];
+            update[i]->next[i] = x;
+        }
+    }
 
 }
 
-void SL_print(SkipList skiplist){
-    return;
+skipListNode SL_find_node(SkipList skiplist, Pointer key, CompareFunc compare){
+    
+    /* first node */
+
+    skipListNode x = skiplist->header;
+
+    /* start from the highest level on the list */
+    for (int i = skiplist->curLevel ; i >= 0 ; --i){
+        while( x->next[i]!=NULL){
+            if( compare(x->next[i]->key , key) >=0 ){
+                break;
+            }
+            x = x->next[i];
+        }
+    }
+
+    if(x->next[1]!=NULL){
+        if (x->next[1]->key!=NULL){
+            if (compare(x->next[1]->key,key)== 0 ){
+
+                return x->next[1];
+            }
+        }
+    }   
+
+    return NULL;
+
+}
+
+Pointer SL_node_key(skipListNode node){
+    if(node->key!= NULL){
+        return node->key;
+    }
+    else{
+        return NULL;
+    }
+}
+
+
+Pointer SL_node_val(skipListNode node){
+    if(node->value != NULL){
+        return node->value;
+    }
+    else{
+        return NULL;
+    }
+}
+
+
+void SL_print(SkipList skiplist, PrintFunc print){
+    for(int i = skiplist->curLevel ; i >=0 ; i--){
+        skipListNode node = skiplist->header->next[i];
+        // printf("Level %d\n", i);
+        while(node !=NULL){
+            Pointer key = node->key;
+            print(key);
+            node = node->next[i];
+        }
+        printf("\n");
+    }
+}
+
+Pointer SL_find(SkipList skiplist, Pointer key, CompareFunc compare){
+    skipListNode node = SL_find_node(skiplist,key,compare);
+    return node == NULL ? NULL : node->value;
+
+}
+
+void SL_remove(SkipList skiplist, Pointer key){
+    
 }
 void SL_destroy(SkipList skiplist){
     return;
